b5dac30693474ea51afd48c14f3e3bb232753f86
abdel <a.naji@hotmail.fr>
2018-11-15 14:06:25 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <meta charset="utf-8">
+    <head>
+        <title>php p1 ex1</title>
+    </head>
+    <body>
+        <?php
+        $name = 'abdel';
+        echo "hello :  $name";
+        ?>
+    </body>
+</html>
